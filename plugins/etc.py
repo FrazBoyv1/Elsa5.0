9532ef96c5d8fce26fc8e7e594d2acc1af6cbb98
FrazBoy <91239698+FrazBoyv1@users.noreply.github.com>
2025-05-25 11:13:18 +0530

    Update etc.py

--- plugins/etc.py
+++ plugins/etc.py
@@ -42,5 +42,5 @@ async def stats(bot, update):
 async def stop_button(bot, message):
     msg = await bot.send_message(text="**𝖡𝗈𝗍 𝖨𝗌 𝖱𝖾𝗌𝗍𝖺𝗋𝗍𝗂𝗇𝗀...🪄**", chat_id=message.chat.id)       
     await asyncio.sleep(3)
-    await msg.edit("**𝖡𝗈𝗍 𝖱𝖾𝗌𝗍𝖺𝗋𝗍𝖾𝖽 𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅𝗅𝗒 ! 𝖱𝖾𝖺𝖽𝗒 𝖳𝗈 𝖬𝗈𝗏𝖾 𝖮𝗇 💯**")
+    await msg.edit("**𝖡𝗈𝗍 𝖱𝖾𝗌𝗍𝖺𝗋𝗍𝖾𝖽 𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅𝗅𝗒 ! @Cyber_Fcracker 🦥**")
     os.execl(sys.executable, sys.executable, *sys.argv)
